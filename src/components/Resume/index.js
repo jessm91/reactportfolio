f249b1a5991a528bb229b0c3c8613114e32b3690
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-04 11:00:27 -0600

    start resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,29 @@
+import React from 'react'; 
+
+function Resume() {
+
+return(
+<section class="mb-5">
+    <h2 className="resume">Resume</h2>
+        <hr></hr>
+        <div class="row justify-content-center" id="resume">
+            <div class="mt-5 pl-5 pr-5">
+                <h2>Jessica Mangiameli</h2>
+               
+               <a href="" class="link">Download Resume</a>
+               
+            </div>
+        </div>
+
+        <div class="justify-content-center mt-5">
+            <div>
+                <h2>Proficiencies</h2>
+                <p>HTML, CSS (Bootstrap, HandleBars), JavaScript, React.js, 
+                    Node.js, Express.js, SQL, Sequelize, NoSQL, APIs</p>
+            </div>
+        </div>
+</section>
+);
+}
+
+export default Resume;
