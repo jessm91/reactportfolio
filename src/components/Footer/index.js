f3a2e5881045b9b362a6b2e1825aec02d69944f7
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-04 10:00:12 -0600

    footer section

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {  
+
+    return (
+    <div className="footer" id="footer">
+        <a href="https://github.com/jessm91">Github</a>
+    </div>
+    )
+}
+
+export default Footer;
