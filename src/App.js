4c5fcb5804ef807eb81bfa3233c38813028e4708
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-04 09:39:16 -0600

    initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
