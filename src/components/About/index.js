ab52bf52de4920cc51d2542bbf96b9445d285cb5
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-04 09:51:21 -0600

    about section

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+    return(
+    <section className="title">
+      <h2 class="name">Jessica Mangiameli</h2>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+        
+        <p>I am a front-end web developer specializing in HTML and CSS. I'm currently located in the midwest and am working on obtaining a certificate in full-stack web development. My hobbies include traveling, reading and cooking. Take a look at my projects and feel free to contact me!
+    </p>
+      </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;
