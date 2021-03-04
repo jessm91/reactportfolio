5af416ab5fb7599253ab4d1d0a48826a06bd445c
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-04 10:48:05 -0600

    portfolio

---
+++ src/components/Project/index.js
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ProjectCards(props) {
+    return (
+        
+      <div className="card"> 
+        <div className="img-container">
+          <img alt={props.name} src={props.image}/>
+        </div>
+        <div className="content">
+              <p className="project-title" id="card-title">{props.name}</p>
+              <p id="proj-icons-container">
+              <a href={props.github}>Github</a> 
+              <a href={props.deploy}>Deployed Application</a>             
+             </p>
+              <p id="topics">
+                    ({props.topics})
+                </p>
+        </div>
+      </div>
+    );
+}
+  
+  export default ProjectCards;
